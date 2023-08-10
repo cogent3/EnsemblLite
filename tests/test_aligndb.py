3cf0525e4727288975dff713db3b936f31013cd3
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-08-10 12:07:50 +0100

    MAINT: change aligndb test to using maf file

--- tests/test_aligndb.py
+++ tests/test_aligndb.py
@@ -1,9 +1,9 @@
 from ensembl_cli.aligndb import AlignDb
-from ensembl_cli.install import _emf_to_gap_records
+from ensembl_cli.install import _load_one_align
 
 
 def test_db_align(DATA_DIR, tmp_path):
-    records = _emf_to_gap_records(DATA_DIR / "sample.emf")
+    records = _load_one_align(DATA_DIR / "tiny.maf")
     outpath = tmp_path / "blah.sqlitedb"
     db = AlignDb(source=outpath)
     db.add_records(records)
