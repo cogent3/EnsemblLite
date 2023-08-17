22452fb960e327c563cbd7820a98fc90da74f047
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-08-17 20:47:01 +0100

    ENH: added rich table display function
    [NEW] converts a cogent3 Table into a rich.table.Table

--- src/ensembl_cli/util.py
+++ src/ensembl_cli/util.py
@@ -387,3 +387,29 @@ def _(sig_path: str) -> Callable:
 
 def get_signature_data(path: os.PathLike) -> Callable:
     return _sig_load_funcs[path.name](path)
+
+
+def rich_display(c3t, title_justify="left"):
+    """converts a cogent3 Table to a Rich Table and displays it"""
+    from cogent3.format.table import formatted_array
+    from rich.console import Console
+    from rich.table import Table
+
+    cols = c3t.columns
+    columns = [formatted_array(cols[c], pad=False)[0] for c in c3t.header]
+    rich_table = Table(
+        title=c3t.title,
+        highlight=True,
+        title_justify=title_justify,
+        title_style="bold blue",
+    )
+    for col in c3t.header:
+        numeric_type = any(v in cols[col].dtype.name for v in ("int", "float"))
+        j = "right" if numeric_type else "left"
+        rich_table.add_column(col, justify=j, no_wrap=numeric_type)
+
+    for row in zip(*columns):
+        rich_table.add_row(*row)
+
+    console = Console()
+    console.print(rich_table)
