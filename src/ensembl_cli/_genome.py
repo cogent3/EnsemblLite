e3f5f8c7de7c3c00d4ae49b0ef7e8d3ce5b9a01c
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-10-09 10:32:19 +1100

    ENH: added a CompressedGenome class
    [NEW] stores compressed sequences in sqlite db. Smaller disk footprint,
        but possibly slower for selecting sequence segments since it has
        to decompressed.

--- src/ensembl_cli/_genome.py
+++ src/ensembl_cli/_genome.py
@@ -1,12 +1,18 @@
 import typing
 
+from cogent3 import get_app
+from cogent3.app.composable import define_app
+
 from ensembl_cli._db_base import SqliteDbMixin
 
 
 OptInt = typing.Optional[int]
 
 
-# make a plugin hook that queries for sequence strings, this is our distributed version
+# todo: make a variant that wraps a directory of compressed sequence files
+# todo: or compresses sequence records on write into db, and just inflates then returns substring
+
+
 class Genome(SqliteDbMixin):
     table_name = "genome"
     _genome_schema = {"coord_name": "TEXT PRIMARY KEY", "seq": "TEXT", "length": "INT"}
@@ -60,3 +66,52 @@ class Genome(SqliteDbMixin):
             values = start, end, coord_name
 
         return self._execute_sql(sql, values).fetchone()[0]
+
+
+@define_app
+def _to_bytes(data: str) -> bytes:
+    return data.encode("utf8")
+
+
+@define_app
+def _from_bytes(data: bytes) -> str:
+    return data.decode("utf8")
+
+
+_compress = _to_bytes() + get_app("compress")
+_decompress = get_app("decompress") + _from_bytes()
+
+
+class CompressedGenome(Genome):
+    _genome_schema = {"coord_name": "TEXT PRIMARY KEY", "seq": "BLOB", "length": "INT"}
+
+    def add_record(self, *, coord_name: str, seq: str):
+        sql = f"INSERT INTO {self.table_name}(coord_name, seq, length) VALUES (?, ?, ?)"
+        self._execute_sql(sql, (coord_name, _compress(seq), len(seq)))
+        self.db.commit()
+
+    def add_records(self, *, records: typing.Iterable[list[str, str]]):
+        sql = f"INSERT INTO {self.table_name}(coord_name, seq, length) VALUES (?, ?, ?)"
+        self.db.executemany(sql, [(n, _compress(s), len(s)) for n, s in records])
+
+    def get_seq(
+        self, *, coord_name: str, start: OptInt = None, end: OptInt = None
+    ) -> str:
+        """
+
+        Parameters
+        ----------
+        coord_name
+            name of chromosome etc..
+        start
+            starting position of slice in python coordinates, defaults
+            to 0
+        end
+            ending position of slice in python coordinates, defaults
+            to length of coordinate
+        """
+        sql = f"SELECT seq FROM {self.table_name} where coord_name = ?"
+
+        seq = _decompress(self._execute_sql(sql, (coord_name,)).fetchone()[0])
+        print(seq)
+        return seq[start:end]
--- tests/test_genome.py
+++ tests/test_genome.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ensembl_cli._genome import Genome
+from ensembl_cli._genome import CompressedGenome, Genome
 
 
 @pytest.fixture(scope="function")
@@ -11,17 +11,27 @@ def small_genome():
     return db, seqs
 
 
+@pytest.fixture(scope="function")
+def compressed_small_genome():
+    seqs = {"s1": "AAACCCCAA", "s2": "TTGGTT"}
+    db = CompressedGenome(source=":memory:", species="dodo")
+    db.add_records(records=seqs.items())
+    return db, seqs
+
+
+@pytest.mark.parametrize("genome", ("small_genome", "compressed_small_genome"))
 @pytest.mark.parametrize(
     "name,start,end", (("s1", 3, 7), ("s1", 3, None), ("s1", None, 7), ("s2", 2, 4))
 )
-def test_get_seq(small_genome, name, start, end):
-    genome, seqs = small_genome
+def test_get_seq(genome, request, name, start, end):
+    genome, seqs = request.getfixturevalue(genome)
     expect = seqs[name][start:end]
     assert genome.get_seq(coord_name=name, start=start, end=end) == expect
 
 
+@pytest.mark.parametrize("genome", ("small_genome", "compressed_small_genome"))
 @pytest.mark.parametrize("name", ("s1", "s2"))
-def test_get_fullseq(small_genome, name):
-    genome, seqs = small_genome
+def test_get_fullseq(genome, request, name):
+    genome, seqs = request.getfixturevalue(genome)
     expect = seqs[name]
     assert genome.get_seq(coord_name=name) == expect
