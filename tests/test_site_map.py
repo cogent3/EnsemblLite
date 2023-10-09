709ed2ae02c11b50e25ed2fc75a3c8bfe1a65b48
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-10-09 13:00:16 +1100

    TST: update tests to new site map class

--- tests/test_site_map.py
+++ tests/test_site_map.py
@@ -3,7 +3,7 @@ import pytest
 from ensembl_cli._site_map import get_site_map
 
 
-@pytest.mark.parametrize("site", ("ftp.ensembl.org", "ftp.ensemblgenomes.ebi.ac.uk"))
+@pytest.mark.parametrize("site", ("ftp.ensembl.org",))
 def test_correct_site(site):
     smap = get_site_map(site)
     assert smap.site == site
@@ -11,4 +11,5 @@ def test_correct_site(site):
 
 def test_standard_smp():
     sm = get_site_map("ftp.ensembl.org")
-    assert str(sm._genomes_path) == "fasta/dna"
+    assert sm.get_seqs_path("abcd") == "fasta/abcd/dna"
+    assert sm.get_annotations_path("abcd") == "gff3/abcd"
