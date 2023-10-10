6faee7c4a47fbded2acd42e899066722cfc64048
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-10-10 12:45:42 +1100

    ENH: added CompressedGenome.add_compressed_records()
    [NEW] provide sequences already compressed to this method

--- src/ensembl_cli/_genome.py
+++ src/ensembl_cli/_genome.py
@@ -95,6 +95,12 @@ class CompressedGenome(Genome):
         sql = f"INSERT INTO {self.table_name}(coord_name, seq, length) VALUES (?, ?, ?)"
         self.db.executemany(sql, [(n, _compress(s), len(s)) for n, s in records])
 
+    def add_compressed_records(self, *, records: typing.Iterable[list[str, bytes]]):
+        """sequences already compressed"""
+        sql = f"INSERT INTO {self.table_name}(coord_name, seq, length) VALUES (?, ?, ?)"
+        self.db.executemany(sql, [(n, s, len(s)) for n, s in records])
+        self.db.commit()
+
     def get_seq(
         self, *, coord_name: str, start: OptInt = None, end: OptInt = None
     ) -> str:
