92e8beaa3ea9e5149c73081302dd3bb295bbd982
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-08-27 18:38:34 +1000

    REL: set semantic version to 0.1a1

--- src/ensembl_lite/__init__.py
+++ src/ensembl_lite/__init__.py
@@ -6,11 +6,4 @@ filterwarnings("ignore", message=".*MPI")
 filterwarnings("ignore", message="Can't drop database.*")
 
 
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2023-, The ensembl lite project"
-__credits__ = ["Gavin Huttley"]
-__license__ = "BSD"
-__version__ = "2023.7.9a1"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "alpha"
+__version__ = "0.1a1"
