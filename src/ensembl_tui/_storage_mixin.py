69e86114522231a8ce0c1d5dd9e592a30e5065ec
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-01-16 09:26:46 +1100

    MAINT: move common duckdb/parquet code to _storage_mixin base class

--- src/ensembl_tui/_storage_mixin.py
+++ src/ensembl_tui/_storage_mixin.py
@@ -3,9 +3,12 @@ import dataclasses
 import functools
 import io
 import os
+import pathlib
 import sqlite3
 
+import duckdb
 import numpy
+import typing_extensions
 
 from ensembl_tui import _util as eti_util
 
@@ -198,3 +201,73 @@ class Hdf5Mixin(eti_util.SerialisableMixin):
                     self._file.close()
 
         self._is_open = False
+
+
+class ViewMixin:
+    _source: pathlib.Path  # override in subclass
+
+    @property
+    def species(self) -> str:
+        return self._source.name
+
+
+@dataclasses.dataclass(slots=True)
+class DuckdbParquetBase:
+    source: dataclasses.InitVar[pathlib.Path]
+    # db is for testing purposes
+    db: dataclasses.InitVar[duckdb.DuckDBPyConnection | None] = None
+    _source: pathlib.Path = dataclasses.field(init=False)
+    _conn: duckdb.DuckDBPyConnection = dataclasses.field(init=False, default=None)  # type: ignore
+    _tables: tuple[str, ...] | tuple = ()
+
+    def __post_init__(
+        self,
+        source: pathlib.Path,
+        db: duckdb.DuckDBPyConnection | None,
+    ) -> None:
+        source = pathlib.Path(source)
+        self._source = source
+        if db:
+            self._conn = db
+            return
+
+        self._conn = None
+
+        if not source.is_dir():
+            msg = f"{self._source} is not a directory"
+            raise OSError(msg)
+
+        if hasattr(self, "_post_init"):
+            self._post_init()
+
+    @property
+    def conn(self) -> duckdb.DuckDBPyConnection:
+        if self._conn is None:
+            self._conn = duckdb.connect(":memory:")
+            for table in self._tables:
+                parquet_file = self._source / f"{table}.parquet"
+                if not parquet_file.exists():
+                    msg = f"{parquet_file} does not exist"
+                    raise FileNotFoundError(msg)
+
+                sql = f"CREATE TABLE {table} AS SELECT * FROM read_parquet('{parquet_file}')"
+                self._conn.sql(sql)
+
+        return self._conn
+
+    def __len__(self) -> int:
+        return self.num_records()
+
+    def __eq__(self, other: typing_extensions.Self) -> bool:
+        return other.conn is self.conn
+
+    @property
+    def source(self) -> pathlib.Path:
+        return self._source
+
+    def close(self) -> None:
+        self.conn.close()
+
+    def num_records(self) -> int:  # pragma: no cover
+        # override in subclass
+        raise NotImplementedError
